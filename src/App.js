b7a6564aad5b12271741e2606d40eaeb666bb923
BLADWING <timoti.ponarenkO@gmail.com>
2022-07-23 01:55:31 +0400

    create validation for login, add show real user, add localstorage...

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
   ];
 
   const errors = {
-    uname: "invalid username",
-    pass: "invalid password",
+    uname: "Incorrect username",
+    pass: "Incorrect password",
   };
 
   const handleSubmit = (event) => {
@@ -27,27 +27,33 @@ function App() {
 
     var { uname, pass } = document.forms[0];
     const userData = database.find((user) => user.username === uname.value);
-
-    switch (userData) {
-      case userData.password !== pass.value:
-        return setErrorMessages({ name: "pass", message: errors.pass });
-      case userData.uname !== uname.value:
-        return setErrorMessages({ name: "uname", message: errors.uname });
-      default:
+    if (userData) {
+      if (userData.password !== pass.value) {
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        Test(userData);
         setIsSubmitted(true);
+      }
+    } else {
+      setErrorMessages({ name: "uname", message: errors.uname });
     }
+  };
 
-    // if (userData) {
-    //   if (userData.password !== pass.value) {
 
-    //     setErrorMessages({ name: "pass", message: errors.pass });
-    //   } else {
-    //     setIsSubmitted(true);
-    //   }
-    // } else {
 
-    //   setErrorMessages({ name: "uname", message: errors.uname });
-    // }
+  //here I try to make limited time....
+  const Test = (userData, ttl) => {
+    const now = new Date();
+    const item = { expiry: now.getTime() + ttl };
+    localStorage.setItem("Login", JSON.stringify([userData.username, item]));
+  };
+
+  const TestName = JSON.parse(localStorage.getItem("Login"));
+
+  const LogOut = (e) => {
+    e.preventDefault();
+    window.location.reload();
+    localStorage.clear();
   };
 
   const renderErrorMessage = (name) =>
@@ -55,18 +61,27 @@ function App() {
       <div className="error">{errorMessages.message}</div>
     );
 
+
+    //Here I try to make real time logged user...
+  const realTime = () => {
+    if (TestName !== []) {
+      setIsSubmitted(true);
+    }
+  };
   const renderForm = (
     <div className="form">
       <form onSubmit={handleSubmit}>
         <div className="input-container">
           <label>Username </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
+          <input type="text" name="uname" />
         </div>
         <div className="input-container">
           <label>Password </label>
-          <input type="password" name="pass" required />
+          <input type="password" name="pass" />
+        </div>
+        <div className="error">
           {renderErrorMessage("pass")}
+          {renderErrorMessage("uname")}
         </div>
         <div className="button-container">
           <input type="submit" />
@@ -79,7 +94,15 @@ function App() {
     <div className="app">
       <div className="login-form">
         <div className="title">Sign In</div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      
+        {isSubmitted ? (
+          <div>
+            User: {TestName[0]} is successfully logged in
+            <button onClick={LogOut}> Log Out </button>
+          </div>
+        ) : (
+          renderForm
+        )}
       </div>
     </div>
   );
